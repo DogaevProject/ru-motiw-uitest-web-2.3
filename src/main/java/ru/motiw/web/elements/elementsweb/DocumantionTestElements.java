072b5d8bdb76ba8ddcb1db49fc3011ef48a67b11
DogaevProject <boysos@icloud.com>
2018-09-02 19:26:59 +0300

    Переработка методов проверки документации

--- src/main/java/ru/motiw/web/elements/elementsweb/DocumantionTestElements.java
+++ src/main/java/ru/motiw/web/elements/elementsweb/DocumantionTestElements.java
@@ -11,45 +11,15 @@ import org.openqa.selenium.support.FindBy;
 
 public  class DocumantionTestElements {
 
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/glossary.pdf\"]")
-    private SelenideElement manualGlossary;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/tips_for_beginners.pdf\"]")
-    private SelenideElement tipsForBeginners;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/admin_do_guide.pdf\"]")
-    private SelenideElement adminDoGuide;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/admin_secure_and_system_guide.pdf\"]")
-    private SelenideElement adminSecureAndSystemGuide;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/install_guide.pdf\"]")
-    private SelenideElement  installGuide;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/api_manual.pdf\"]")
-    private SelenideElement  apiManual;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/User_Guide.pdf\"]")
-    private SelenideElement  userGuide;
-
-    @FindBy(xpath = "//p/a[@href=\"/FAQ/arm_manual.pdf\"]")
-    private SelenideElement  armManual;
-
     @FindBy(xpath = "//embed[@type='application/pdf']")
     private SelenideElement checkPdfManualPage;
 
     @FindBy(xpath = "//p/a[contains(@href,  'pdf')]")
     private ElementsCollection allReferenceToPdf;
 
-    @FindBy(xpath = "//*[text()=\"Далее\" or text()=\"1 Глоссарий\"]") //ссылка с текстом "Далее" во html-фрейме на большенстве страницах или ссылка с текстом "1 Глоссарий" на странице "Термины и определения".
+    @FindBy(xpath = "//*[text()=\"Далее\" or text()=\"1 Глоссарий\" or text()=\"Вперед →\"]") //ссылка с текстом "Далее" во html-фрейме на большенстве страницах или ссылка с текстом "1 Глоссарий" на странице "Термины и определения".
     private SelenideElement checkHtmlManualPage;
 
-    /*
-    * Коллекция всех элементов c текстом в мануале
-    */
-    @FindBy (xpath = "//td[@align=\"left\" and  not(contains(@class,\"toc\")) and @style]//span")
-    private ElementsCollection allTextOnHtmlManual;
 
     @FindBy(xpath = "//p/a[contains(@href,  'html')]")
     private ElementsCollection allReferenceToHtml;
@@ -58,45 +28,6 @@ public  class DocumantionTestElements {
     private SelenideElement frameHtml;
 
 
-    @FindBy(xpath = "//p[contains(text(),'Советы для начинающих ')]")
-    private ElementsCollection namesOfManuals;
-//div[@class="in"]
-
-    @FindBy(xpath = "(//p/a[contains(@href,  'html')])[2]")
-    private SelenideElement test;
-
-
-
-
-    public SelenideElement getManualGlossary() { return manualGlossary; }
-
-    public  SelenideElement getManualTipsForBeginners() {
-        return tipsForBeginners;
-    }
-
-    public SelenideElement getAdminDoGuide() {
-        return adminDoGuide;
-    }
-
-    public SelenideElement getAdminSecureAndSystemGuide() {
-        return adminSecureAndSystemGuide;
-    }
-
-    public SelenideElement getInstallGuide() {
-        return installGuide;
-    }
-
-    public SelenideElement getApiManual() {
-        return apiManual;
-    }
-
-    public SelenideElement getUserGuide() {
-        return userGuide;
-    }
-
-    public SelenideElement getArmManual() {
-        return armManual;
-    }
 
     //наличе элемента отвечающего за отображение pdf
     public  SelenideElement getPdfPage() {
@@ -117,18 +48,11 @@ public  class DocumantionTestElements {
         return allReferenceToHtml;
     }
 
-    //Весь текст в мануале
-    public  ElementsCollection getAllTextOnHtmlManual () {return allTextOnHtmlManual; }
 
     public SelenideElement getFrameHtml(){
         return frameHtml;
     }
 
-    public ElementsCollection getNamesOfManuals(){
-        return namesOfManuals;
-    }
-    public SelenideElement test() {
-        return test;
-    }
+
 }
 
--- src/main/java/ru/motiw/web/steps/Administration/DocumentionTestSteps.java
+++ src/main/java/ru/motiw/web/steps/Administration/DocumentionTestSteps.java
@@ -1,10 +1,9 @@
 package ru.motiw.web.steps.Administration;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.motiw.web.elements.elementsweb.DocumantionTestElements;
 import ru.motiw.web.steps.BaseSteps;
 
@@ -12,9 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.codeborne.selenide.Selenide.*;
-import static org.testng.Assert.assertEquals;
-import static ru.motiw.web.model.NamesOfManuals.CREATE_TASK;
-import static ru.motiw.web.model.NamesOfManuals.DICTIONARY;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static ru.motiw.utils.WindowsUtil.newWindowForm;
+import static ru.motiw.utils.WindowsUtil.selectLinkOpenInNewTab;
 import static ru.motiw.web.model.URLMenu.MANUALS;
 
 /**
@@ -23,184 +24,23 @@ import static ru.motiw.web.model.URLMenu.MANUALS;
 public class DocumentionTestSteps extends BaseSteps {
 
     private DocumantionTestElements documantionTestElements = page(DocumantionTestElements.class);
-   // private NamesOfManuals namesOfManuals = page(NamesOfManuals.class);
+
 
     /*
-    *  Проверка Загрузки pdf-страницы
-    * */
-    private DocumentionTestSteps ensurePdfPageLoaded()  {
+     *  Проверка Загрузки pdf-страницы
+     * */
+    private void ensurePageLoaded()  {
         documantionTestElements.getPdfPage().shouldBe(Condition.visible);
-        return this;
     }
-
-     /*
-    *  Проверка Загрузки html-страницы
-    * */
-    private DocumentionTestSteps ensureHtmlPageLoaded() {
-       documantionTestElements.getHtmlPage().shouldBe(Condition.visible);
-        return this;
-    }
-
-
-
-
-
 
 
     /*
-    *  Находим все ссылки на pdf-мануалы. Проверяем кол-во найденных ссылок. Переходим и проверяем загрузку pdf-страницы.
-    * */
+     *  Проверка Загрузки html-страницы
+     * */
 
-    private DocumentionTestSteps checkPdfManuals() {
-        //проверяем кол-во ссылок на pdf-мануалы
-        documantionTestElements.getAllReferenceToPdfManual().shouldHaveSize(14);
-        //Находим все ссылки на pdf-мануалы и помещаем их в массив.
-        List<SelenideElement> elements = new ArrayList<>();
-        for (SelenideElement selenideElement : documantionTestElements.getAllReferenceToPdfManual()) {
-            elements.add(selenideElement);
-        }
-        for (int i = 0; i < elements.size(); i++) {
-            elements.get(i).click();//Переходим по найденным ссылкам на pdf-мануалы.
-            ensurePdfPageLoaded(); //Проверяем Загрузку pdf-страницы
-            goToURLManuals();
-            sleep(5000);
-        }
-
-        return this;
+    private void ensureHtmlPageLoaded() {
+        documantionTestElements.getHtmlPage().shouldBe(Condition.visible);
     }
-    /*
-    *  Находим все ссылки на html-мануалы. Проверяем кол-во найденных ссылок. Переходим и проверяем загрузку html-страницы.
-    * */
-
-    private DocumentionTestSteps checkHtmlManuals() {
-        //проверяем кол-во ссылок на html-руководства
-        documantionTestElements.getAllReferenceToHtmlManual().shouldHaveSize(14);
-        //Находим все ссылки на html-мануалы и помещаем их в массив.
-        List<SelenideElement> elements = new ArrayList<>();
-
-        for (SelenideElement selenideElement : documantionTestElements.getAllReferenceToHtmlManual()) {
-            elements.add(selenideElement);
-
-        }
-        for (int i = 0; i < elements.size(); i++) {
-            sleep(500);
-            elements.get(i).click(); //Переходим по найденным ссылкам на html-мануалы.
-            //проверяем наличие фрейма - он есть на всех страницах, за исключением страницы "Термины и определения"
-            if (isElementPresent(By.xpath("//frame[@name='hmcontent']"))){
-            getFrameObject($(documantionTestElements.getFrameHtml())); //  уходим во фрейм hmcontent
-            ensureHtmlPageLoaded();//Проверяем Загрузку html-страницы
-            //checkTextOnHtmlPage();   // Проверяем наличе текста, который отображается на страницах html-мануала
-            } else
-                ensureHtmlPageLoaded(); // на странице "Термины и определения" сразу проверяем загрузку, без перехода во фрейм (его там нет)
-           isElementPresent1();//TEST
-            goToURLManuals();
-        }
-
-        return this;
-    }
-
-
-//TEST
-    private  boolean isElementPresent1() {
-        try {
-            isElementPresent(By.xpath("//frame[@name='hmcontent']"));
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    //тестововый!!
-    /*
-    private DocumentionTestSteps checkingNameOfManuals() {
-
-        for (int i = 0; i <mm.size(); i++) {
-            mm.get(i).toString(); //Переходим по найденным ссылкам на html-мануалы.
-            //проверяем наличие фрейма - он есть на всех страницах, за исключением страницы "Термины и определения"
-            if (isElementPresent(By.xpath("//frame[@name='hmcontent']"))){
-                getFrameObject($(documantionTestElements.getFrameHtml())); //  уходим во фрейм hmcontent
-                ensureHtmlPageLoaded();//Проверяем Загрузку html-страницы
-                checkTextOnHtmlPage();   // Проверяем наличе текста, который отображается на страницах html-мануала
-            }
-
-
-        //где-то должна быть коллекция названий
-        List<String> elements = new ArrayList<>();
-        for (SelenideElement selenideElement : documantionTestElements.getAllReferenceToHtmlManual()) {
-            elements.add((String) selenideElement);
-        }
-
-      /*
-      //Забрать весь текст со страницы.
-      //берем все элементы в div-е
-        for (SelenideElement strings: $$(By.xpath("//div[@class=\"in\"]"))) {
-            String text = strings.getText(); //переводим в строку
-            System.out.println(text);
-        }
-
-
-
-        //в div  ищем все имена из коллекции
-        //$$(By.xpath("//div[@class=\"in\"]")).shouldHave(CollectionCondition.texts("Термины и определения "));
-
-        return this;
-    }*/
-
-//универсальный метод сравнение текста из коллекции
-    private static void Qqq(ElementsCollection collection, java.lang.String text) {
-
-
-        for (SelenideElement e : collection) {
-            e.getText().equals(text);
-            e.shouldBe(Condition.visible);
-            }
-
-        }
-
-
-    private static void www(ElementsCollection collection, java.lang.String textValue) {
-
-        for (SelenideElement e : collection) {
-            if (e.getText().equals(textValue)) {
-                e.shouldBe(Condition.visible);
-                assertEquals("asdasd", DICTIONARY.getNameOfOfManual());
-            }
-
-        }
-    }
-
-    private DocumentionTestSteps eee() {
-        for (SelenideElement strings : $$(By.xpath("//div[@class=\"in\"]"))) {
-            java.lang.String text = strings.getText(); //переводим в строку
-            assertEquals(
-                    "Термины и определения pdf html\n" +
-                    "Советы для начинающих pdf html\n" +
-                    "Руководство администратора документооборота pdf html\n" +
-                    "Руководство администратора системы pdf html\n" +
-                    "Руководство по установке и обслуживанию системы МОТИВ pdf html\n" +
-                    "Руководство программиста pdf html\n" +
-                    "Руководство пользователя системы МОТИВ pdf html\n" +
-                    "Руководство пользователя программы «Информер» pdf html\n" +
-                    "Руководство пользователя программы XConductor pdf html\n" +
-                    "Руководство пользователя. Мобильный интерфейс pdf html\n" +
-                    "Руководство пользователя. Новый мобильный интерфейс pdf html\n" +
-                    "Руководство по внедрению системы МОТИВ pdf html\n" +
-                    "Установка и настройка КриптоПро ЭЦП pdf html\n" +
-                    "Руководство пользователя программы ScanMaster (потоковое сканирование) pdf html", text);
-
-        }
-        return this;
-    }
-
-        //сравнение текста
-    private DocumentionTestSteps diff() {
-        www(documantionTestElements.getNamesOfManuals(), CREATE_TASK.getNameOfOfManual());
-
-        return this;
-    }
-
-
-
 
 
 
@@ -212,24 +52,69 @@ public class DocumentionTestSteps extends BaseSteps {
         return page(DocumentionTestSteps.class);
     }
 
+
+
     /*
-    *  Открываем руководство, Проверяем Загрузку pdf,html-страницы
-    * */
+     *  Открываем руководство, Проверяем Загрузку pdf-страницы, Переход в раздел "Руководства"
+     * */
 
-    public DocumentionTestSteps checkManuals() {
-       // documantionTestElements.test().click();
-// посмотри for в методах - что-то е так
-        //ensureHtmlPageLoaded();
 
-       checkPdfManuals();
-       checkHtmlManuals();
-        //eee();
-        //diff();
-        //checkingNameOfManuals();
-        //test();
-        //downloadsFilesx();
-        //*checkPdfManuals();
-        return this;
+    private void checkPdfManuals() {
+        //проверяем кол-во ссылок на pdf-мануалы
+        documantionTestElements.getAllReferenceToPdfManual().shouldHaveSize(14);
+        String parentWindowHandler = getWebDriver().getWindowHandle(); //Запоминаем окно в котором находимся
+        //все ссылки на pdf добавляем в список
+        List<SelenideElement> elements = new ArrayList<>($$(documantionTestElements.getAllReferenceToPdfManual()));
+
+        for (SelenideElement link:elements) {
+            link.sendKeys(selectLinkOpenInNewTab);
+            getWebDriver().switchTo().window(new WebDriverWait(getWebDriver(), 10).until(newWindowForm(By.xpath("//embed[@type='application/pdf']"))));
+            ensurePageLoaded();
+            closeWindow();
+            getWebDriver().switchTo().window(parentWindowHandler); // Возращаемся в окно страницы /user/tab/user/manuals
+            switchTo().frame($(By.cssSelector("#flow"))); //Переходим во фрейм на странице /user/tab/user/manuals
+
+        }
+
+
+    }
+
+
+    private void checkHtmlManuals() {
+
+        //проверяем кол-во ссылок на html-руководства
+        documantionTestElements.getAllReferenceToHtmlManual().shouldHaveSize(14);
+        String parentWindowHandler = getWebDriver().getWindowHandle(); //Запоминаем окно в котором находимся
+        //все ссылки на html добавляем в список
+        List<SelenideElement> elements = new ArrayList<>($$(documantionTestElements.getAllReferenceToHtmlManual()));
+
+        for (SelenideElement link:elements) {
+            link.sendKeys(selectLinkOpenInNewTab);
+            ArrayList<String> tabs2 = new ArrayList<> (getWebDriver().getWindowHandles());
+            switchTo().window(tabs2.get(1));
+
+            {
+                if (isElementPresent(By.xpath("//frame[@name='hmcontent']"))) {
+                    getFrameObject($(documantionTestElements.getFrameHtml())); //  уходим во фрейм hmcontent
+                    ensureHtmlPageLoaded();//Проверяем Загрузку html-страницы
+                    //checkTextOnHtmlPage();   // Проверяем наличе текста, который отображается на страницах html-мануала
+                }
+                ensureHtmlPageLoaded(); // на страницах "Термины и определения" и "Описание таблиц базы данных" сразу проверяем загрузку, без перехода во фрейм (его там нет)
+            }
+            closeWindow();
+            getWebDriver().switchTo().window(parentWindowHandler); // Возращаемся в окно страницы /user/tab/user/manuals
+            switchTo().frame($(By.cssSelector("#flow"))); //Переходим во фрейм на странице /user/tab/user/manuals
+
+        }
+
+
+
+    }
+
+
+    public void checkManuals() {
+        checkPdfManuals();
+        checkHtmlManuals();
     }
 
 
